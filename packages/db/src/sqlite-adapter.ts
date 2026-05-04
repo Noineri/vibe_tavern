070b639937c5f6fb2d5bf52a244da5e8be24c813
Noineri <lady.gofercia@gmail.com>
2026-05-04 07:47:00 +0300

    B-DM7: cleanup legacy db and providers

--- packages/db/src/sqlite-adapter.ts
+++
@@ -1,9 +0,0 @@
-export type SqliteValue = string | number | null;
-export type SqliteRow = Record<string, SqliteValue>;
-
-export interface SqliteDatabaseAdapter {
-  execute(sql: string, params?: SqliteValue[]): void;
-  queryAll<T extends SqliteRow>(sql: string, params?: SqliteValue[]): T[];
-  queryOne<T extends SqliteRow>(sql: string, params?: SqliteValue[]): T | null;
-  transaction<T>(callback: () => T): T;
-}
