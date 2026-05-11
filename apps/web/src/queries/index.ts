8173c30e3df3f3390deae358946d7318966efad3
Noineri <lady.gofercia@gmail.com>
2026-05-11 08:14:15 +0300

    refactor: task TQ1 - TanStack Query foundation + provider profile queries

---
+++ apps/web/src/queries/index.ts
@@ -0,0 +1,17 @@
+export { providerKeys, chatKeys, characterKeys, personaKeys, bootstrapKeys } from "./query-keys.js";
+export {
+  useProviderProfilesQuery,
+  useProviderModelsQuery,
+  useFavoriteModelsQuery,
+  useSaveProviderProfileMutation,
+  useUpdateProviderProfileMutation,
+  useDeleteProviderProfileMutation,
+  useActivateProviderProfileMutation,
+  useTestProviderProfileMutation,
+  useTestProviderDraftMutation,
+  useTestProfileChatMutation,
+  useTestProviderChatMutation,
+  useFetchModelsByEndpointMutation,
+  useToggleFavoriteModelMutation,
+  useRefreshProviderProfilesMutation,
+} from "./provider-queries.js";
